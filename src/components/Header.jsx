8667de9e779d26f75604e8584f29236b43eeffd1
Gauravs1104 <gsawarn132654@gmail.com>
2025-08-14 00:24:56 +0530

    responsive change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header>
       <nav>
         <div className="left">Gaurav's Portfolio</div>
-        <div className="right">
-          <ul>
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className={`right ${isMenuOpen ? 'open' : ''}`}>
+          <ul onClick={closeMenu}>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#services">Services</a></li>
